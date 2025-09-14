91fac8904cd833acf635a47b6624a3b96b536a15
Shubham Phalke <phalkeshubham2003@gmail.com>
2025-09-14 18:00:44 +0530

    Employee Model change

--- BackEnd/validators/employee.validator.js
+++ BackEnd/validators/employee.validator.js
@@ -3,6 +3,10 @@ import { z } from 'zod';
 export const employeeSchema = z.object({
   username: z.string().min(3, "Username must be at least 3 characters long"),
   password: z.string().min(6, "Password must be at least 6 characters long"),
+  name: z.string().min(1, "Name is required"),
+  contact: z.string().regex(/^[6-9]\d{9}$/, "Must be a valid 10-digit Indian number"),
+  alternateContact: z.string().regex(/^[6-9]\d{9}$/, "Must be 10-digit number").optional(),
+  email: z.string().email("Invalid email"),
   type: z.enum(["Admin", "BackDesk", "Onsite"]),
   status: z.enum(["Active", "Inactive"]),
 });
